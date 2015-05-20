9e252ae55fa104c8e415c067e451560938deb30c
Bharathwaj Sankara <sankara.bharathwaj@gmail.com>
2015-05-19 22:42:39 -0700

    minor

--- bin/CreateCopyWrHdr.py
+++ bin/CreateCopyWrHdr.py
@@ -1,21 +1,23 @@
 #!/usr/local/bin/python3
 
+#
+# Script to create headers
+#
+
 import sys
 import os
 import shutil
 
-print ("Processing ", sys.argv[1])
 f = open(sys.argv[1])
 newf = ".", sys.argv[1]
 fn = sys.argv[1].split('/')
-print("HOO: fnsize:", len(fn))
 d = open(".trail_name", 'w')
 d.write("/*++++++++++++++++++++++++++++++++++++++++++++++++\n")
 d.write(" * File:   ");
 d.write(fn[len(fn)-1])
 d.write("\n * Author: Bharathwaj Sankara\n")
 d.write(" * wajsankara at yahoo dot com")
-d.write("\n *+++++++++++++++++++++++++++++++++++++++++++++++++\n\n\n")
+d.write("\n *++++++++++++++++++++++++++++++++++++++++++++++++*/\n\n\n")
 
 for l in f:
   d.write(l);
